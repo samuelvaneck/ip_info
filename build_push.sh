e697e0fa772d200fa204520b1617f9dc74dc5952
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-07-29 13:23:26 +0200

    Change repository

--- build_push.sh
+++ build_push.sh
@@ -1,4 +1,4 @@
-docker build -t ghcr.io/samuelvaneck/ip:latest .
+docker build -t ghcr.io/samuelvaneck/ip_info:latest .
 echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
-docker tag ghcr.io/samuelvaneck/ip:latest
-docker push ghcr.io/samuelvaneck/ip:latest
+docker tag ghcr.io/samuelvaneck/ip_info:latest
+docker push ghcr.io/samuelvaneck/ip_info:latest
