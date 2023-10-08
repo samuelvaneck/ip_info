be933fd1418764aae6e09d8d59fefb8f5a5c65e8
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-10-08 17:43:38 +0200

    Fix build push script

--- build_push.sh
+++ build_push.sh
@@ -2,5 +2,4 @@ set -xe
 
 docker build -t ghcr.io/samuelvaneck/ip_info:latest .
 echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
-docker tag ghcr.io/samuelvaneck/ip_info:latest
 docker push ghcr.io/samuelvaneck/ip_info:latest
