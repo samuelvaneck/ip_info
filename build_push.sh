f6eb5b557222707720e2b103582af678e442bac9
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-10-08 17:33:39 +0200

    Add other platforms

--- build_push.sh
+++ build_push.sh
@@ -1,3 +1,5 @@
+set -xe
+
 docker build -t ghcr.io/samuelvaneck/ip_info:latest .
 echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
 docker tag ghcr.io/samuelvaneck/ip_info:latest
