d54c3a9f13b64957c262841bc6f39ebc66ca1521
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-04-12 12:47:08 +0200

    Fix docker build with platform

--- build_push.sh
+++ build_push.sh
@@ -1,5 +1,5 @@
 set -xe
 
-docker build -t ghcr.io/samuelvaneck/ip_info:latest .
+docker build --platform linux/amd64 -t ghcr.io/samuelvaneck/ip_info:latest .
 echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
 docker push ghcr.io/samuelvaneck/ip_info:latest
